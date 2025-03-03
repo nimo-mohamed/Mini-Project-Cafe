aa06ed378151956eb6db3aaabc7795458244c315
Nimo Mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-03-03 12:46:29 +0000

    created my menuItem with sealed traits

--- src/main/scala/MenuItem.scala
+++ src/main/scala/MenuItem.scala
@@ -2,8 +2,15 @@
 // Premium specials that can be able to be added/removed from the menu.
 // Stock count of the menu items. Some cannot order an item if the count is zero.
 
-class MenuItem {
-
-  // nimo was here
-  println("Hi")
+sealed trait MenuCategory
+object MenuCategory {
+  case object Food extends MenuCategory
+  case object Drink extends MenuCategory
 }
+
+case class MenuItem(
+                   name: String,
+                   price: Double,
+                   category: MenuCategory,
+                   isPremium: Boolean
+                   )
